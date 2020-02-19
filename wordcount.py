01099c754fdabadf6f6f320c2e1b23dca41b14f5
slukas111 <sashamati@gmail.com>
2020-02-19 11:28:16 -0800

    written comment for student time

--- wordcount.py
+++ wordcount.py
@@ -55,6 +55,8 @@ if sys.version_info[0] >= 3:
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 
+####$$$ Took several days, wanted to get this down. Command line help from Stew w. class vid.
+
 def build_dictionary(filename): #helper
     dictionary = {}
 #f = (filename, 'r')
@@ -86,7 +88,7 @@ def  print_top(filename):
         word = top_word[0]
         count = top_word[1]
         print(word, count)
-
+##printing out the first 20
 
 def main():
     if len(sys.argv) != 3:
